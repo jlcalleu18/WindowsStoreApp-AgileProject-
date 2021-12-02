241f2e517dbfc1dd499f3b9826b3aee9bf169d50
Jorge Calle <jlcalleu18@gmail.com>
2021-12-02 17:34:58 -0500

    connecting with the dataBase

--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,7 +17,8 @@ namespace Agile_Project
         {
             InitializeComponent();
         }
-
+        SqlConnection myconn;
+        string id, email, password, firstName, lastName;
         private void label1_Click(object sender, EventArgs e)
         {
         }
@@ -34,6 +36,63 @@ namespace Agile_Project
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            //Establish a connection with the DBMS
+
+            myconn = new SqlConnection();
+            myconn.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\jorgecalle\\source\\repos\\Agile-Project\\data\\DB-Users.mdf;Integrated Security=True";
+            //MessageBox.Show("open DB");
+            //Build a command object to hold the SQL statement 
+            SqlCommand mycommand = new SqlCommand();
+
+            try
+            {
+                if (txtEmail.Text != "" && txtPassword.Text != "")
+                {
+
+                
+    
+                    myconn.Open();
+                    mycommand.Connection = myconn;
+                    mycommand.CommandText = "SELECT *FROM Users WHERE email=@email and password=@password";
+                    mycommand.Parameters.AddWithValue("@email", txtEmail.Text);
+                    mycommand.Parameters.AddWithValue("@password", txtPassword.Text);
+                    
+                    SqlDataReader reader;
+                    reader = mycommand.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            id = reader["id"].ToString();
+                            email = reader["email"].ToString();
+                            password = reader["password"].ToString();
+                            firstName = reader["firstName"].ToString();
+                            lastName = reader["lastName"].ToString();
+                         
+                        }
+                        MessageBox.Show("Data found your name is " + firstName + " " + lastName);
+                        txtEmail.Text = "";
+                        txtPassword.Text="";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data not found", "Information");
+                        txtPassword.Text = "";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Username or Password is empty", "Information");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Connection Error", "Information");
+            }
         }
     }
 }
