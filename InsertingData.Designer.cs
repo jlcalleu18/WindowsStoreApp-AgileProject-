f32b214bcba1d2f57378e2bfdf42f0141da1a5fc
Jorge Calle <jlcalleu18@gmail.com>
2021-12-12 16:18:58 -0500

    creating an insetdata page

---
+++ InsertingData.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace Agile_Project
+{
+    partial class InsertingData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddData = new System.Windows.Forms.Button();
+            this.txtProducts = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.txtBrand = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.imageBox = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAddData
+            // 
+            this.btnAddData.Location = new System.Drawing.Point(527, 966);
+            this.btnAddData.Name = "btnAddData";
+            this.btnAddData.Size = new System.Drawing.Size(150, 46);
+            this.btnAddData.TabIndex = 0;
+            this.btnAddData.Text = "Add Data";
+            this.btnAddData.UseVisualStyleBackColor = true;
+            // 
+            // txtProducts
+            // 
+            this.txtProducts.Location = new System.Drawing.Point(239, 82);
+            this.txtProducts.Name = "txtProducts";
+            this.txtProducts.Size = new System.Drawing.Size(388, 39);
+            this.txtProducts.TabIndex = 1;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(239, 397);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(388, 39);
+            this.txtPrice.TabIndex = 2;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(239, 471);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(388, 423);
+            this.txtDescription.TabIndex = 3;
+            // 
+            // txtType
+            // 
+            this.txtType.Location = new System.Drawing.Point(239, 315);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(388, 39);
+            this.txtType.TabIndex = 5;
+            // 
+            // txtModel
+            // 
+            this.txtModel.Location = new System.Drawing.Point(239, 241);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(388, 39);
+            this.txtModel.TabIndex = 6;
+            // 
+            // txtBrand
+            // 
+            this.txtBrand.Location = new System.Drawing.Point(239, 173);
+            this.txtBrand.Name = "txtBrand";
+            this.txtBrand.Size = new System.Drawing.Size(388, 39);
+            this.txtBrand.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 32);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Product Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 32);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Brand";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 32);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Model";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 322);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 32);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Type";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 404);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 32);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Price";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 474);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(135, 32);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Description";
+            // 
+            // imageBox
+            // 
+            this.imageBox.Location = new System.Drawing.Point(673, 266);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(509, 466);
+            this.imageBox.TabIndex = 14;
+            this.imageBox.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(867, 198);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 32);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Image";
+            // 
+            // InsertingData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1218, 1048);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.imageBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBrand);
+            this.Controls.Add(this.txtModel);
+            this.Controls.Add(this.txtType);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtProducts);
+            this.Controls.Add(this.btnAddData);
+            this.Name = "InsertingData";
+            this.Text = "InsertingData";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddData;
+        private System.Windows.Forms.TextBox txtProducts;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.TextBox txtType;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.TextBox txtBrand;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox imageBox;
+        private System.Windows.Forms.Label label7;
+    }
+}
